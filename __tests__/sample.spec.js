6bba1c2a273edb293900428bad9e92296c5aa3ac
Joseph Gordy <jgordy2424@gmail.com>
2020-01-17 22:52:58 -0500

    More config for global test variables like 'describe', 'it', 'expect'

--- __tests__/sample.spec.js
+++ __tests__/sample.spec.js
@@ -1,5 +1,3 @@
-/* global describe, it, expect */
-
 describe('Sample Test', () => {
     it('should test that true === true', () => {
         expect(true).toBe(true);
--- controllers/tests/users.spec.js
+++ controllers/tests/users.spec.js
@@ -1,4 +1,3 @@
-/* global describe, it, beforeAll, expect */
 const request = require('supertest');
 const app = require('../../server');
 
