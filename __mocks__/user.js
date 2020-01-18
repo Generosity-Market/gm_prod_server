bec6694f1559f3ccdc75abb0ff6cb299423cfbc4
Joseph Gordy <jgordy2424@gmail.com>
2020-01-18 12:17:23 -0500

    Add user mock data for testing

---
+++ __mocks__/user.js
@@ -0,0 +1,31 @@
+module.exports = {
+    details: {
+        email: 'test_user@testemail.com',
+        first_name: 'Testy',
+        last_name: 'Tester',
+        address_1: '123 Testing Dr',
+        address_2: 'Unit D',
+        city: 'Atlanta',
+        state: 'GA',
+        zipcode: '30350',
+        coverImage: 'jessica-lewis-512219.jpg',
+        profileImage: 'profile2.jpg',
+    },
+    passwords: {
+        password: 'TestPassword_0123',
+        confirmPassword: 'TestPassword_0123',
+    },
+    Preferences: {
+        round_image: true,
+        white_text: true,
+    },
+    editedDetails: {
+        phone: '5551234567',
+        address_1: '987 Tester Circle',
+    },
+    editedPreferences: {
+        Preferences: {
+            white_text: false,
+        },
+    },
+};
