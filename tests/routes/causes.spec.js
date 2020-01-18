6326b8dafddcb5af7ffbf448f22b7d6c03cc40c8
Joseph Gordy <jgordy2424@gmail.com>
2020-01-18 17:04:05 -0500

    Add test todos on cause routes

--- tests/routes/causes.spec.js
+++ tests/routes/causes.spec.js
@@ -1,6 +1,6 @@
 // const request = require('supertest');
 // const app = require('../../server');
-// const { user } = require('../../__mocks__');
+// const { cause, user } = require('../../__mocks__');
 
 // const {
 //     createItem,
@@ -21,5 +21,41 @@
 describe('Cause Endpoints', () => {
     it('should pass a test', () => {
         expect(true).toEqual(true);
+    });
+
+    describe('[POST] - /api/causes', () => {
+        it.todo('should return status code 201 (Created)');
+
+        it.todo('should return a newly created cause');
+
+        it.todo('should contain a cause\'s preferences');
+    });
+
+    describe('[GET] - /api/causes', () => {
+        it.todo('should return status code 200 (ok)');
+
+        it.todo('should return at least one cause');
+
+        it.todo('should return a cause object');
+
+        it.todo('should contain a cause\'s preferences');
+    });
+
+    describe('[GET] - /api/causes/:id', () => {
+        it.todo('should return status code 200 (ok)');
+
+        it.todo('should return exactly one cause');
+
+        it.todo('should return a cause object');
+
+        it.todo('should contain a cause\'s preferences');
+    });
+
+    describe('[PUT] - /api/causes/:id', () => {
+        it.todo('should return status code 201 (created)');
+
+        it.todo('should return a cause with updated data');
+
+        it.todo('should contain a cause\'s preferences');
     });
 });
