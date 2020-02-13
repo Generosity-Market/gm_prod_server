8eeddc220a929a5cc430e95e3698a7d68d4e5220
Joseph Gordy <jgordy2424@gmail.com>
2020-02-12 23:27:12 -0500

    Cleanup routes/middleware

--- server.js
+++ server.js
@@ -15,8 +15,14 @@ const {
 } = require('./src/auth/middleware');
 const authConfig = require('./src/auth/config');
 
+// Custom Middleware
 const {
-    handleError,
+    corsPolicy,
+} = require('./src/middleware');
+
+// Utilities
+const {
+    error,
     logger,
 } = require('./src/utilities');
 
@@ -34,19 +40,19 @@ app.use(passport.initialize());
 passport.use(new BasicStrategy(verifyUser));
 passport.use(new JwtStrategy(authConfig.options, verifyToken));
 
+// Allows CORS
+app.use(corsPolicy);
+
 app.use(routes);
 
 app.use((err, req, res) => {
-    handleError(err, res);
+    error.handleError(err, res);
 });
 
 // NOTE: This conditional is excluded during testing
 if (require.main === module) {
     const port = app.get('port');
-    app.listen(port, () => {
-        // eslint-disable-next-line no-console
-        logger.success('Node app is running on port', port);
-    });
+    app.listen(port, () => logger.success('Node app is running on port', port));
 }
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,47 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const passport = require('passport');
-const { BasicStrategy } = require('passport-http');
+const swaggerUi = require('swagger-ui-express');
 
+// Route collections
 const documentation = require('./documentation');
 const users = require('./users');
 const causes = require('./causes');
-// const donations = require('./donations');
+const donations = require('./donations');
 // const comments = require('./comments');
 // const organizations = require('./organizations');
 // const preferences = require('./preferences');
 
-const {
-    corsPolicy,
-} = require('../middleware');
-
 const router = express.Router();
 
-// Passport Basic Authentication Strategy
-passport.use(new BasicStrategy(
-    (username, password, done) => {
-        const userPassword = users[username];
-
-        if (!userPassword) return done(null, false);
-        if (userPassword !== password) return done(null, false);
-
-        return done(null, username);
-    },
-));
-
-// Allows CORS
-router.use(corsPolicy);
-
-router.use('/', documentation);
-router.use('/docs', documentation);
-router.use('/api', documentation);
-router.use('/api/docs', documentation);
+router.use('/api/docs', swaggerUi.serve, documentation);
 router.use('/api/users', users);
 router.use('/api/causes', causes);
-// router.use('/api/donations', donations);
+router.use('/api/donations', donations);
 // router.use('/api/comments', comments);
 // router.use('/api/organizations', organizations);
 // router.use('/api/preferences', preferences);
-
+router.use('/', swaggerUi.serve, documentation);
+router.use('/docs', swaggerUi.serve, documentation);
+router.use('/api', (req, res) => res.status(200).send('See /api/docs for more info'));
+router.use('*', (req, res) => res.status(404).send('Not found'));
 
 module.exports = router;
