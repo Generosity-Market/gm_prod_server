38c25e9df4dbc43d91fb831bcf15bd7cf9c72c52
Joseph Gordy <jgordy2424@gmail.com>
2020-02-14 21:13:46 -0500

    FIx CORS issue to accept the Authorization header

--- src/middleware/corsPolicy.js
+++ src/middleware/corsPolicy.js
@@ -1,6 +1,7 @@
 const corsPolicy = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 };
 
