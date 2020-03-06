0ac5616a34e59815a669f1b99bfa5c946b8cb6c0
Joseph Gordy <jgordy2424@gmail.com>
2020-03-06 01:56:04 -0500

    Revert header changes, change allow origin value

--- src/middleware/corsPolicy.js
+++ src/middleware/corsPolicy.js
@@ -1,8 +1,7 @@
 const corsPolicy = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    // eslint-disable-next-line max-len
-    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 };
 
