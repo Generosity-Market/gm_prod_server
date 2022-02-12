06c5af2eab0f2eb153b70b34229541c6d6b8d338
Joseph Gordy <jgordy2424@gmail.com>
2022-02-12 16:45:34 -0500

    Add heroku app to allowed domains

--- src/middleware/corsPolicy.js
+++ src/middleware/corsPolicy.js
@@ -1,5 +1,9 @@
 const corsPolicy = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+    const allowedDomains = ['http://localhost:3001', 'https://generosity-market.herokuapp.com/'];
+    const { origin } = req.headers;
+    if (allowedDomains.indexOf(origin) > -1) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
