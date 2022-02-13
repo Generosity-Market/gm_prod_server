1b2fe23db85944b35240282332abaac34579700c
Joseph Gordy <jgordy2424@gmail.com>
2022-02-13 14:56:13 -0500

    Remove console.logs

--- src/middleware/corsPolicy.js
+++ src/middleware/corsPolicy.js
@@ -1,9 +1,8 @@
 const corsPolicy = (req, res, next) => {
     const allowedDomains = ['http://localhost:3001', 'https://generosity-market.herokuapp.com'];
     const { origin } = req.headers;
-    console.log('Origin: ', origin);
+
     if (allowedDomains.indexOf(origin) > -1) {
-        console.log('Add Access-Control-Allow-Origin header');
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
